309c36a1ebee63dad5521b8f5aa25e189d58ad80
gashki <20312509+gashki@users.noreply.github.com>
2019-07-15 22:30:12 -0700

    Added custom scroll bar to the sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Sidebar.css";
+
+// Custom scroll bar library
+import "../../lib/simplebar.min.css";
+import SimpleBar from "../../lib/simplebar.min.js";
 
 // React components
 import Collections from "./Collections";
 import Filters from "./Filters";
 
 
-function Sidebar({ contentState, changeState }) {
-  const collections = <Collections />;
-  const filters = <Filters contentState={contentState} changeState={changeState} />;
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div className="sidebar">
-      <div className="sidebar-inner">
-        <SidebarSection title="Collections" content={collections} />
-        <SidebarSection title="Filters" content={filters} />
+  componentDidMount() {
+    new SimpleBar(document.getElementById("sidebar-simplebar"));
+  }
+
+  render() {
+    const { contentState, changeState } = this.props;
+
+    const collections = <Collections />;
+    const filters = <Filters contentState={contentState} changeState={changeState} />;
+
+    return (
+      <div id="sidebar-simplebar" className="sidebar">
+        <div className="sidebar-inner">
+          <SidebarSection title="Collections" content={collections} />
+          <SidebarSection title="Filters" content={filters} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 
